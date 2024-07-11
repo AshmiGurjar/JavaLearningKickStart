ab5e46b8e416e05c145f9f8597da8442d45ad2d0
AshmiGurjar <Ashmi.dgurjar@gmail.com>
2024-07-11 11:03:06 +0530

    Initial Commit

---
+++ src/Lab001.java
@@ -0,0 +1,8 @@
+public class Lab001 {
+    public static void main(String[] args) {
+        //System.out.println(2+2);
+        int age = 5;
+        age = age +2;
+        System.out.println(age);
+    }
+}
